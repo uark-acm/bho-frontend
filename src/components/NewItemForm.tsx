9912ace8613720109347a79d9cbb67c2f9a73d9e
Jack Norris <jacktimothynorris@gmail.com>
2022-04-06 09:53:36 -0500

    First commit

---
+++ src/components/NewItemForm.tsx
@@ -0,0 +1,9 @@
+import { FunctionComponent } from "react"
+
+type NewItemFormProps = {}
+
+const NewItemForm: FunctionComponent<NewItemFormProps> = (props: NewItemFormProps) => {
+    return <></>;
+}
+
+export default NewItemForm;
