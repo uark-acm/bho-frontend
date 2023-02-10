a29caa0a7f7273c8b56dc3341ac4b4e72746a4e1
a-mufasa <ahmedr.moustafa@gmail.com>
2023-02-10 14:35:47 -0600

    ClothingItem -> BHOItem from data models

--- src/redux/actions/ClothingItems.actions.ts
+++ src/redux/actions/ClothingItems.actions.ts
@@ -1,19 +1,19 @@
 import { Dispatch } from "react";
 import { Action } from "./types";
-import { ClothingItem, CreateClothingItemRequest } from '@uark-acm/bho-data-models/lib';
+import { BHOItem, BHOItemCreateRequest } from '@uark-acm/bho-data-models/lib';
 import Loadable from "../redux-config/loadable";
 import { rootURL } from "../../config/endpoints";
 
 type ClothingItemAction = {
 	type: Action;
-	payload: Loadable<ClothingItem[]>;
+	payload: Loadable<BHOItem[]>;
 };
 
 export const fetchClothingItems = () => {
     return async (dispatch: Dispatch<ClothingItemAction>) => {
         dispatch({type: Action.FETCH_CLOTHING_ITEMS, payload: {status: 'loading'}});
         try {
-            const data: ClothingItem[] = await (await fetch(`${rootURL}/clothingItems`)).json();
+            const data: BHOItem[] = await (await fetch(`${rootURL}/clothingItems`)).json();
             dispatch({type: Action.FETCH_CLOTHING_ITEMS, payload: {status: 'success', data: data}});
         }
         catch(error){
@@ -23,7 +23,7 @@ export const fetchClothingItems = () => {
     }
 }
 
-export const createClothingItems = (createClothingItemRequest: CreateClothingItemRequest) => {
+export const createClothingItems = (BHOItemCreateRequest: BHOItemCreateRequest) => {
     return async (dispatch: Dispatch<ClothingItemAction>) => {
 
     }
