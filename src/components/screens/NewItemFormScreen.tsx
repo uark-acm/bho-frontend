3b379a7882dfc5b9fe754b55162ba917aeb902a6
Rohit <kalarohit9@gmail.com>
2023-02-10 15:30:37 -0600

    created basic form

--- src/components/screens/NewItemFormScreen.tsx
+++ src/components/screens/NewItemFormScreen.tsx
@@ -1,9 +1,32 @@
-import { FunctionComponent } from "react"
+import React, { useState, FunctionComponent } from "react";
 
 type NewItemFormScreenProps = {}
 
 const NewItemFormScreen: FunctionComponent<NewItemFormScreenProps> = (props: NewItemFormScreenProps) => {
-    return <>New Item Shit</>;
+    const [itemValues, setItemValues] = useState({
+        itemName: "",
+        itemCategory: "",
+        itemSize: "",
+        itemDescription: "",
+        itemPhoto: -1,
+    })
+
+    const changeHandler = e => {
+        setItemValues({...itemValues, [e.target.name]: e.target.value})
+    }
+
+    return(
+        <form name="createClothingItemForm">
+            <label>Item Name</label>
+            <input type="text" id="itemName" name="itemName" onChange={changeHandler} required></input>
+            <label>Item Category</label>
+            <datalist id="itemCategory" name="itemCategory" onChange={changeHandler}></datalist>
+            <label>Item Size</label>
+            <datalist id="itemSize" name="itemSize" onChange={changeHandler}></datalist>
+            <label>Item Description</label>
+            <input id="itemDescription" name="itemDescription" onChange={changeHandler} required></input>
+        </form>
+    )
 }
 
 export default NewItemFormScreen;
