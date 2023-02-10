f981ede50fe61af78cb5231149014071fcbcaa30
Rohit <kalarohit9@gmail.com>
2023-02-10 15:51:03 -0600

    added photo field

--- src/components/screens/NewItemFormScreen.tsx
+++ src/components/screens/NewItemFormScreen.tsx
@@ -8,7 +8,7 @@ const NewItemFormScreen: FunctionComponent<NewItemFormScreenProps> = (props: New
         itemCategory: "",
         itemSize: "",
         itemDescription: "",
-        itemPhoto: -1,
+        itemPhoto: "",
     })
 
     const changeHandler = e => {
@@ -25,6 +25,9 @@ const NewItemFormScreen: FunctionComponent<NewItemFormScreenProps> = (props: New
             <datalist id="itemSize" name="itemSize" onChange={changeHandler}></datalist>
             <label>Item Description</label>
             <input id="itemDescription" name="itemDescription" onChange={changeHandler} required></input>
+            {/* TODO: make the photo input a file selector and change data type to allow people to upload photos*/}
+            <label>Photo</label>
+            <input id="itemPhoto" name ="itemPhoto" onChange={changeHandler} required></input>
         </form>
     )
 }
