2699ee1181ccce84f4bb16eb964e695e026c4b72
Rohit <kalarohit9@gmail.com>
2023-02-17 16:00:54 -0600

    form is done

--- src/components/screens/NewItemFormScreen.tsx
+++ src/components/screens/NewItemFormScreen.tsx
@@ -1,12 +1,18 @@
 import React, { useState, FunctionComponent } from "react";
 import { useFormControl } from '@mui/material/FormControl';
 import { Grid, Container, Select, TextField } from "@mui/material";
-import {Box} from "@mui/system";
+import {Box, BoxProps} from "@mui/system";
+import {alpha, styled} from "@mui/material/styles"
+import { Margin } from "@mui/icons-material";
+import './NewItemFormScreen.css';
+
 
 
  
 
 type NewItemFormScreenProps = {}
+
+
 
 const NewItemFormScreen: FunctionComponent<NewItemFormScreenProps> = (props: NewItemFormScreenProps) => {
     const [itemValues, setItemValues] = useState({
@@ -16,6 +22,8 @@ const NewItemFormScreen: FunctionComponent<NewItemFormScreenProps> = (props: New
         itemDescription: "",
         itemPhoto: "",
     })
+
+    const TextFieldBoxesSx = {mb: '10%', ml: '5%'};
 
     const changeHandler = (e: React.ChangeEvent<any>) => {
         setItemValues({...itemValues, [e.target.id]: e.target.value})
@@ -27,34 +35,48 @@ const NewItemFormScreen: FunctionComponent<NewItemFormScreenProps> = (props: New
 
     return(
         <Grid container spacing={10}>
-            <Grid item xs={2}>
-                <label>Admin/Add Inventory</label>
+            <Grid item xs={4}>
+                <Box sx={{height: '20%', mt: '20%', ml:'5%'}}>
+                    <label className="gray-label">Admin / Add Inventory</label>
+                </Box>
+            
                 <form name="createClothingItemForm">
-                    <label>Item Name</label> <br/>
-                    <TextField size ="small" variant="filled" id="itemName" name="itemName" onChange={changeHandler} required hiddenLabel fullWidth ></TextField> <br />
-                    <label>Item Category</label> <br />
-                    <TextField select id="itemCategory" name="itemCategory" variant="filled" onChange={changeHandler}  hiddenLabel fullWidth>
-                        <Select required></Select> 
-                    </TextField>
-                    <br />
+                    <Box sx={TextFieldBoxesSx}>
+                        <label >Item Name</label>
+                        <TextField size ="small" variant="filled" id="itemName" name="itemName" onChange={changeHandler} required hiddenLabel fullWidth ></TextField>
+                    </Box>
+                    
+                    <Box sx={TextFieldBoxesSx}>
+                        <label>Item Category</label>
+                        <TextField size="small" select id="itemCategory" name="itemCategory" variant="filled" onChange={changeHandler}  hiddenLabel fullWidth>
+                            <Select required></Select> 
+                        </TextField>
+                    </Box>
 
-                    <label>Item Size</label> <br />
-                    <TextField  id="itemSize" name="itemSize" select  variant="filled" hiddenLabel fullWidth onChange={changeHandler}>
-                        <Select required></Select>
-                    </TextField> <br />
-                    <label>Item Description</label> <br />
-                    <TextField size="small" variant="filled" id="itemDescription" name="itemDescription" onChange={changeHandler} required  multiline minRows= {4} hiddenLabel fullWidth></TextField> <br />
+                    <Box sx={TextFieldBoxesSx}>
+                        <label>Item Size</label> 
+                        <TextField  size="small" id="itemSize" name="itemSize" select  variant="filled" hiddenLabel fullWidth onChange={changeHandler}>
+                            <Select required></Select>
+                        </TextField> <br />
+                    </Box>
                     
-                    
+                    <Box sx={TextFieldBoxesSx}>
+                        <label>Description</label>
+                        <TextField size="small" variant="filled" id="itemDescription" name="itemDescription" onChange={changeHandler} required  multiline minRows= {4} hiddenLabel fullWidth></TextField> <br />
+                    </Box>
                 </form>
             </Grid>
-            <Grid item xs = {2}>
-                <Box  maxWidth="sm" sx={{  display: 'flex', height: '40%', alignItems: 'center',justifyContent: 'center', margin: 'auto',  p: 2, border: '2px dashed grey' , borderRadius: 3, fontSize: '15px', flexDirection: 'column'}}>
+
+            <Grid item xs = {2} sx={{ml:'10%', mt:'15%', }}>
+                <Box  maxWidth="sm" sx={{display: 'flex', width: '100%', height: '50%', alignItems: 'center',justifyContent: 'center', margin: 'auto',  p: 2, border: '2px dashed grey' , borderRadius: 3, fontSize: '15px', flexDirection: 'column'}}>
                     <label>Select an item photo and drop here</label>
-                    <input id="itemPhoto" name ="itemPhoto" onChange={changeHandler} required></input>
-                    <label>JPG, PNG or PDF, file size no more than 10MB</label>
+                    <label className="gray-label">JPG, PNG or PDF, file size no more than 10MB</label>
+                    <button>Select File</button>
                 </Box>
-                <input type="submit"></input> 
+                <Box sx={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                    <button className="add-item-button" >Add Item </button> 
+                </Box>
+                
             </Grid>
     </Grid> 
     )
