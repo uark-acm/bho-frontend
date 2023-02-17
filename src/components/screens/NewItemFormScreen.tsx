605fa0442bbdd031eff4c911c5cc8bdfc9487b66
Rohit <kalarohit9@gmail.com>
2023-02-17 16:10:44 -0600

    added finishing touches on the form by making it more aligned with what is shown in the mock

--- src/components/screens/NewItemFormScreen.tsx
+++ src/components/screens/NewItemFormScreen.tsx
@@ -23,7 +23,7 @@ const NewItemFormScreen: FunctionComponent<NewItemFormScreenProps> = (props: New
         itemPhoto: "",
     })
 
-    const TextFieldBoxesSx = {mb: '10%', ml: '5%'};
+    const textFieldBoxesSx = {mb: '10%', ml: '5%'};
 
     const changeHandler = (e: React.ChangeEvent<any>) => {
         setItemValues({...itemValues, [e.target.id]: e.target.value})
@@ -36,38 +36,38 @@ const NewItemFormScreen: FunctionComponent<NewItemFormScreenProps> = (props: New
     return(
         <Grid container spacing={10}>
             <Grid item xs={4}>
-                <Box sx={{height: '20%', mt: '20%', ml:'5%'}}>
+                <Box sx={{mb:'10%', mt: '10%', ml:'5%'}}>
                     <label className="gray-label">Admin / Add Inventory</label>
                 </Box>
             
                 <form name="createClothingItemForm">
-                    <Box sx={TextFieldBoxesSx}>
+                    <Box sx={textFieldBoxesSx}>
                         <label >Item Name</label>
                         <TextField size ="small" variant="filled" id="itemName" name="itemName" onChange={changeHandler} required hiddenLabel fullWidth ></TextField>
                     </Box>
                     
-                    <Box sx={TextFieldBoxesSx}>
+                    <Box sx={textFieldBoxesSx}>
                         <label>Item Category</label>
                         <TextField size="small" select id="itemCategory" name="itemCategory" variant="filled" onChange={changeHandler}  hiddenLabel fullWidth>
                             <Select required></Select> 
                         </TextField>
                     </Box>
 
-                    <Box sx={TextFieldBoxesSx}>
+                    <Box sx={textFieldBoxesSx}>
                         <label>Item Size</label> 
                         <TextField  size="small" id="itemSize" name="itemSize" select  variant="filled" hiddenLabel fullWidth onChange={changeHandler}>
                             <Select required></Select>
                         </TextField> <br />
                     </Box>
                     
-                    <Box sx={TextFieldBoxesSx}>
+                    <Box sx={textFieldBoxesSx}>
                         <label>Description</label>
                         <TextField size="small" variant="filled" id="itemDescription" name="itemDescription" onChange={changeHandler} required  multiline minRows= {4} hiddenLabel fullWidth></TextField> <br />
                     </Box>
                 </form>
             </Grid>
 
-            <Grid item xs = {2} sx={{ml:'10%', mt:'15%', }}>
+            <Grid item xs = {2} sx={{ml:'10%', mt:'7.48%', }}>
                 <Box  maxWidth="sm" sx={{display: 'flex', width: '100%', height: '50%', alignItems: 'center',justifyContent: 'center', margin: 'auto',  p: 2, border: '2px dashed grey' , borderRadius: 3, fontSize: '15px', flexDirection: 'column'}}>
                     <label>Select an item photo and drop here</label>
                     <label className="gray-label">JPG, PNG or PDF, file size no more than 10MB</label>
