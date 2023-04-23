b9fc9ba17cad8d304179736b41a89327fa6dd90e
dmroberts302 <dmr019@uark.edu>
2023-04-23 02:04:20 -0500

    Added review changes

--- src/screens/CheckInOutScreen.tsx
+++ src/screens/CheckInOutScreen.tsx
@@ -1,31 +1,8 @@
 import { FunctionComponent } from 'react';
 import { useState, useRef } from 'react';
 import { QrReader } from 'react-qr-reader';
-import styled from 'styled-components';
 import React from 'react';
-
-const Custom = styled.button`
-    font-size: 20px;
-    border: none;
-    color: white;
-    padding: 5px 50px;
-    text-align: center;
-    text-decoration: none;
-    display: block;
-    margin: 0px 2px;
-    cursor: pointer;
-    border-radius: 40px;
-    font-family: monospace;
-    font-weight: 700;
-    margin: auto;
-`;
-
-const camStyle = {
-    display: 'flex',
-    justifyContent: 'center',
-    marginTop: '-20px',
-    marginBottom: '-25px',
-};
+import { Button, Typography } from '@mui/material';
 
 type CheckInOutScreenProps = {};
 
@@ -40,33 +17,40 @@ const CheckInOutScreen: FunctionComponent<CheckInOutScreenProps> = () => {
 
     return (
         <React.Fragment>
-            <div style={camStyle}>
-                <QrReader
-                    onResult={(result, error) => {
-                        if (result && scanItemRef.current) {
-                            console.log(result.getText());
-                            setButtonColor('#A51E36');
-                            scanItemRef.current = false;
-                        }
+            <QrReader
+                onResult={(result, error) => {
+                    if (result && scanItemRef.current) {
+                        console.log(result.getText());
+                        setButtonColor('#A51E36');
+                        scanItemRef.current = false;
+                    }
 
-                        if (error) {
-                            // console.log(error);
-                        }
+                    if (error) {
+                        // console.log(error);
+                    }
+                }}
+                constraints={{ facingMode: 'user' }}
+                containerStyle={{
+                    width: '45%',
+                    margin: 'auto',
+                    marginTop: '-20px',
+                    marginBottom: '-25px',
+                }}
+            />
+            <Typography align="center">
+                <Button
+                    variant="contained"
+                    style={{
+                        borderRadius: '40px',
+                        backgroundColor: buttonColor,
+                        padding: '0.5% 10%',
                     }}
-                    constraints={{ facingMode: 'user' }}
-                    containerStyle={{
-                        width: '45%',
-                        margin: 'auto',
-                    }}
-                />
-            </div>
-
-            <Custom
-                onClick={handleScanItem}
-                style={{ backgroundColor: buttonColor }}
-            >
-                Scan Item
-            </Custom>
+                    disabled={scanItemRef.current}
+                    onClick={handleScanItem}
+                >
+                    Scan Item
+                </Button>
+            </Typography>
         </React.Fragment>
     );
 };
